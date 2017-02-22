e2a93263b370f2bb721eb3531591c34b5da2797a
Toby Clemson <tobyclemson@gmail.com>
2017-02-22 23:11:19 +0000

    Add shorthand methods for invoking commands.

--- lib/ruby_terraform.rb
+++ lib/ruby_terraform.rb
@@ -1,15 +1,42 @@
+require 'ruby_terraform/version'
+require 'ruby_terraform/commands'
+
 module RubyTerraform
   class << self
     attr_accessor :configuration
-  end
 
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
+    def configure
+      @configuration ||= Configuration.new
+      yield(@configuration)
+    end
 
-  def self.reset!
-    self.configuration = nil
+    def clean(opts = {})
+      Commands::Clean.new.execute(opts)
+    end
+
+    def get(opts = {})
+      Commands::Get.new.execute(opts)
+    end
+
+    def apply(opts = {})
+      Commands::Apply.new.execute(opts)
+    end
+
+    def destroy(opts = {})
+      Commands::Destroy.new.execute(opts)
+    end
+
+    def remote_config(opts = {})
+      Commands::RemoteConfig.new.execute(opts)
+    end
+
+    def output(opts = {})
+      Commands::Output.new.execute(opts)
+    end
+
+    def reset!
+      @configuration = nil
+    end
   end
 
   class Configuration
@@ -20,6 +47,3 @@ module RubyTerraform
     end
   end
 end
-
-require 'ruby_terraform/version'
-require 'ruby_terraform/commands'
