f8025479ca439a7c41e6f0b98c95d69562736a34
Toby Clemson <tobyclemson@gmail.com>
2018-08-23 23:13:55 +0100

    Add manual release job to workflow.

---
+++ scripts/ci/steps/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+[ -n "$DEBUG" ] && set -x
+set -e
+set -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$( cd "$SCRIPT_DIR/../../.." && pwd )"
+
+cd "$PROJECT_DIR"
+
+set +e
+openssl aes-256-cbc \
+    -d \
+    -in ./.circleci/gpg.private.enc -k "${ENCRYPTION_PASSPHRASE}" | gpg --import -
+set -e
+
+git crypt unlock
+
+git config --global user.email "circleci@infrablocks.io"
+git config --global user.name "Circle CI"
+
+mkdir -p ~/.gem
+cp config/secrets/rubygems/credentials ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
+
+./go publish:release
+
+git push
