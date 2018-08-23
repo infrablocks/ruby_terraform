7cec4a2fe8b50a75a184a2b761935a2fe96b5d91
Toby Clemson <tobyclemson@gmail.com>
2018-08-23 22:48:57 +0100

    Install git-crypt from native package.

--- scripts/ci/common/install-git-crypt.sh
+++ scripts/ci/common/install-git-crypt.sh
@@ -4,25 +4,5 @@
 set -e
 set -o pipefail
 
-GIT_CRYPT_VERSION="0.5.0"
-
 apt-get update
-apt-get install -y --no-install-recommends git ssh
-
-for key in \
-  EF5D84C1838F2EB6D8968C0410378EFC2080080C \
-; do \
-  gpg --verbose --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
-done
-
-curl -SLO "https://www.agwa.name/projects/git-crypt/downloads/git-crypt-$GIT_CRYPT_VERSION.tar.gz"
-curl -SLO "https://www.agwa.name/projects/git-crypt/downloads/git-crypt-$GIT_CRYPT_VERSION.tar.gz.asc"
-gpg --batch --verify "git-crypt-$GIT_CRYPT_VERSION.tar.gz.asc" "git-crypt-$GIT_CRYPT_VERSION.tar.gz"
-
-mkdir -p /usr/src/git-crypt
-tar -xzf "git-crypt-$GIT_CRYPT_VERSION.tar.gz" -C /usr/src/git-crypt --strip-components=1
-rm "git-crypt-$GIT_CRYPT_VERSION.tar.gz"
-
-cd /usr/src/git-crypt
-make
-make install
+apt-get install -y --no-install-recommends git ssh git-crypt
