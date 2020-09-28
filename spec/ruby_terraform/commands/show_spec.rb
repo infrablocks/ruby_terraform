dba9d11843e2ea8e4d6885e95f46f5c546f6ff19
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:08:39 +0100

    Fix show_spec offenses

--- spec/ruby_terraform/commands/show_spec.rb
+++ spec/ruby_terraform/commands/show_spec.rb
@@ -1,70 +1,101 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe RubyTerraform::Commands::Show do
-  before(:each) do
+  before do
     RubyTerraform.configure do |config|
       config.binary = 'path/to/binary'
     end
   end
 
-  after(:each) do
+  after do
     RubyTerraform.reset!
   end
 
   it 'calls the terraform show command passing the supplied directory' do
-    command = RubyTerraform::Commands::Show.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform show some/path/to/terraform/configuration', any_args))
+    command = described_class.new(binary: 'terraform')
 
     command.execute(path: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform show some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'defaults to the configured binary when none provided' do
-    command = RubyTerraform::Commands::Show.new
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary show some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new
 
     command.execute(path: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'path/to/binary show some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'passes the update option as true when the update option is true' do
-    command = RubyTerraform::Commands::Show.new
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary show -module-depth=0 some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    message = 'path/to/binary show -module-depth=0 ' \
+              'some/path/to/terraform/configuration'
+    command = described_class.new
 
     command.execute(
-        path: 'some/path/to/terraform/configuration',
-        module_depth: 0)
+      path:         'some/path/to/terraform/configuration',
+      module_depth: 0
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'includes the no-color flag when the no_color option is true' do
-    command = RubyTerraform::Commands::Show.new
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary show -no-color some/path/to/terraform/configuration', any_args))
+    command = described_class.new
 
     command.execute(
-        path: 'some/path/to/terraform/configuration',
-        no_color: true)
+      path:     'some/path/to/terraform/configuration',
+      no_color: true
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'path/to/binary show -no-color some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'includes the json flag when the json option is true' do
-    command = RubyTerraform::Commands::Show.new
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary show -json some/path/to/terraform/configuration', any_args))
+    command = described_class.new
 
     command.execute(
-        path: 'some/path/to/terraform/configuration',
-        json: true
+      path: 'some/path/to/terraform/configuration',
+      json: true
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'path/to/binary show -json some/path/to/terraform/configuration',
+          any_args
+        )
     )
   end
 end
