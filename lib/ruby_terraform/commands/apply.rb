1312bd1a6b3f42e8719a040f86a84a793fae0ae2
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:40:59 +0100

    Fix apply command offenses

--- lib/ruby_terraform/commands/apply.rb
+++ lib/ruby_terraform/commands/apply.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'lino'
 require_relative 'base'
@@ -16,34 +18,30 @@ module RubyTerraform
         state = opts[:state]
         input = opts[:input]
         auto_approve = opts[:auto_approve]
-        no_backup = opts[:no_backup]
-        backup = no_backup ? '-' : opts[:backup]
+        backup = opts[:no_backup] ? '-' : opts[:backup]
         no_color = opts[:no_color]
 
         builder
-            .with_subcommand('apply') do |sub|
-              vars.each do |key, value|
-                var_value = value.is_a?(String) ? value : JSON.generate(value)
-                sub = sub.with_option(
-                    '-var', "'#{key}=#{var_value}'", separator: ' ')
-              end
-              sub = sub.with_option('-var-file', var_file) if var_file
-              var_files.each do |file|
-                sub = sub.with_option('-var-file', file)
-              end
-              sub = sub.with_option('-target', target) if target
-              targets.each do |file|
-                sub = sub.with_option('-target', file)
-              end
-              sub = sub.with_option('-state', state) if state
-              sub = sub.with_option('-input', input) if input
-              sub = sub.with_option('-auto-approve', auto_approve) unless
-                  auto_approve.nil?
-              sub = sub.with_option('-backup', backup) if backup
-              sub = sub.with_flag('-no-color') if no_color
-              sub
-            end
-            .with_argument(plan || directory)
+          .with_subcommand('apply') do |sub|
+          vars.each do |key, value|
+            var_value = value.is_a?(String) ? value : JSON.generate(value)
+            sub = sub.with_option(
+              '-var', "'#{key}=#{var_value}'", separator: ' '
+            )
+          end
+          sub = sub.with_option('-var-file', var_file) if var_file
+          var_files.each { |file| sub = sub.with_option('-var-file', file) }
+          sub = sub.with_option('-target', target) if target
+          targets.each { |file| sub = sub.with_option('-target', file) }
+          sub = sub.with_option('-state', state) if state
+          sub = sub.with_option('-input', input) if input
+          sub = sub.with_option('-auto-approve', auto_approve) unless
+            auto_approve.nil?
+          sub = sub.with_option('-backup', backup) if backup
+          sub = sub.with_flag('-no-color') if no_color
+          sub
+        end
+          .with_argument(plan || directory)
       end
     end
   end
