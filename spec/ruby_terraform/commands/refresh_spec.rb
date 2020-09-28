f1b80275390ba20cd8942930b383cf01ec8a167d
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:11:47 +0100

    Fix refresh_spec offenses

--- spec/ruby_terraform/commands/refresh_spec.rb
+++ spec/ruby_terraform/commands/refresh_spec.rb
@@ -1,194 +1,259 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe RubyTerraform::Commands::Refresh do
-  before(:each) do
+  before do
     RubyTerraform.configure do |config|
       config.binary = 'path/to/binary'
     end
   end
 
-  after(:each) do
+  after do
     RubyTerraform.reset!
   end
 
   it 'calls the terraform refresh command passing the supplied directory' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform refresh some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(directory: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform refresh some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'defaults to the configured binary when none provided' do
-    command = RubyTerraform::Commands::Refresh.new
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary refresh some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new
 
     command.execute(directory: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'path/to/binary refresh some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds a var option for each supplied var' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = "terraform refresh -var 'first=1' -var 'second=two' " \
+              'some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      vars:      {
+        first:  1,
+        second: 'two'
+      }
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var 'first=1' -var 'second=two' some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        vars: {
-            first: 1,
-            second: 'two'
-        })
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'correctly serialises list/tuple vars' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var 'list=[1,\"two\",3]' some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        vars: {
-            list: [1, "two", 3]
-        })
+      directory: 'some/configuration',
+      vars:      {
+        list: [1, 'two', 3]
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          "terraform refresh -var 'list=[1,\"two\",3]' some/configuration",
+          any_args
+        )
+    )
   end
 
   it 'correctly serialises map/object vars' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = "terraform refresh -var 'map={\"first\":1,\"second\":\"two\"}' " \
+              'some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      vars:      {
+        map: {
+          first:  1,
+          second: 'two'
+        }
+      }
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var 'map={\"first\":1,\"second\":\"two\"}' some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        vars: {
-            map: {
-                first: 1,
-                second: "two"
-            }
-        })
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'correctly serialises vars with lists/tuples of maps/objects' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = "terraform refresh -var 'list_of_maps=[{\"key\":\"value\"}," \
+               "{\"key\":\"value\"}]' some/configuration"
+    command.execute(
+      directory: 'some/configuration',
+      vars:      {
+        list_of_maps: [
+          { key: 'value' },
+          { key: 'value' }
+        ]
+      }
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var 'list_of_maps=[{\"key\":\"value\"},{\"key\":\"value\"}]' some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        vars: {
-            list_of_maps: [
-                {key: "value"},
-                {key: "value"}
-            ]
-        })
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'adds a state option if a state path is provided' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -state=some/state.tfstate some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        state: 'some/state.tfstate')
+      directory: 'some/configuration',
+      state:     'some/state.tfstate'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform refresh -state=some/state.tfstate some/configuration',
+          any_args
+        )
+    )
   end
 
   it 'includes the no-color flag when the no_color option is true' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform refresh -no-color some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/path/to/terraform/configuration',
-        no_color: true)
+      directory: 'some/path/to/terraform/configuration',
+      no_color:  true
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform refresh -no-color some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds a var-file option for each element of var-files array' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform refresh -var-file=some/vars1.tfvars ' \
+              '-var-file=some/vars2.tfvars some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      var_files: [
+        'some/vars1.tfvars',
+        'some/vars2.tfvars'
+      ]
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var-file=some/vars1.tfvars -var-file=some/vars2.tfvars some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        var_files: [ 
-            'some/vars1.tfvars',
-            'some/vars2.tfvars'
-        ])
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'ensures that var_file and var_files options work together' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform refresh -var-file=some/vars.tfvars -var-file=some/' \
+              'vars1.tfvars -var-file=some/vars2.tfvars some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      var_file:  'some/vars.tfvars',
+      var_files: [
+        'some/vars1.tfvars',
+        'some/vars2.tfvars'
+      ]
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -var-file=some/vars.tfvars -var-file=some/vars1.tfvars -var-file=some/vars2.tfvars some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        var_file: 'some/vars.tfvars',
-        var_files: [ 
-            'some/vars1.tfvars',
-            'some/vars2.tfvars'
-        ])
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'adds a target option if a target is provided' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -target=some_resource_name some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        target: 'some_resource_name')
+      directory: 'some/configuration',
+      target:    'some_resource_name'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform refresh -target=some_resource_name some/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds a target option for each element of target array' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform refresh -target=some_resource_1 ' \
+              '-target=some_resource_2 some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      targets:   [
+        'some_resource_1',
+        'some_resource_2'
+      ]
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -target=some_resource_1 -target=some_resource_2 some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        targets: [
-            'some_resource_1',
-            'some_resource_2'
-        ])
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'ensures that target and targets options work together' do
-    command = RubyTerraform::Commands::Refresh.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform refresh -target=some_resource_1 ' \
+              '-target=some_resource_2 -target=some_resource_3 ' \
+              'some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      target:    'some_resource_1',
+      targets:   [
+        'some_resource_2',
+        'some_resource_3'
+      ]
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform refresh -target=some_resource_1 -target=some_resource_2 -target=some_resource_3 some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        target: 'some_resource_1',
-        targets: [
-            'some_resource_2',
-            'some_resource_3'
-        ])
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 end
