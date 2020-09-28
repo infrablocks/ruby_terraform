2a0b8a8ff5d26375c51757e90bd7f0da74ba1b14
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:04:18 +0100

    Fix spec_helper offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
-require "bundler/setup"
+# frozen_string_literal: true
 
-require "ruby_terraform"
+require 'bundler/setup'
+
+require 'ruby_terraform'
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
