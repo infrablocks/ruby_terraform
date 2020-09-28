099b3052f6e2b7cb9bd842d7b8b44df99f2374fc
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:12:34 +0100

    Fix plan_spec offenses

--- spec/ruby_terraform/commands/plan_spec.rb
+++ spec/ruby_terraform/commands/plan_spec.rb
@@ -1,38 +1,50 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
+require 'English'
 
 describe RubyTerraform::Commands::Plan do
-  before(:each) do
+  before do
     RubyTerraform.configure do |config|
       config.binary = 'path/to/binary'
       config.logger = Logger.new(StringIO.new)
     end
   end
 
-  after(:each) do
+  after do
     RubyTerraform.reset!
   end
 
   it 'calls the terraform plan command passing the supplied directory' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform plan some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(directory: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with('terraform plan some/path/to/terraform/configuration', any_args)
+    )
   end
 
   it 'defaults to the configured binary when none provided' do
-    command = RubyTerraform::Commands::Plan.new
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary plan some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new
 
     command.execute(directory: 'some/path/to/terraform/configuration')
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'path/to/binary plan some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
-  it 'logs the command being executed at debug level using the globally configured logger by default' do
+  it 'logs the command being executed at debug level using ' \
+     'the globally configured logger by default' do
+    allow(Open4).to receive(:spawn)
     string_output = StringIO.new
     logger = Logger.new(string_output)
     logger.level = Logger::DEBUG
@@ -43,239 +55,332 @@ describe RubyTerraform::Commands::Plan do
 
     stub_open4_spawn
 
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
+    command = described_class.new(binary: 'terraform')
 
     command.execute(directory: 'some/path/to/terraform/configuration')
 
     expect(string_output.string).to(
-        include('DEBUG').and(
-            include("Running 'terraform plan some/path/to/terraform/configuration'.")))
+      include('DEBUG').and(
+        include(
+          "Running 'terraform plan some/path/to/terraform/configuration'."
+        )
+      )
+    )
   end
 
-  it 'logs the command being executed at debug level using the provided logger' do
+  it 'logs the command being executed at debug level ' \
+     'using the provided logger' do
+    allow(Open4).to receive(:spawn)
     string_output = StringIO.new
     logger = Logger.new(string_output)
     logger.level = Logger::DEBUG
 
     stub_open4_spawn
 
-    command = RubyTerraform::Commands::Plan.new(
-        binary: 'terraform', logger: logger)
+    command = described_class.new(
+      binary: 'terraform', logger: logger
+    )
 
     command.execute(directory: 'some/path/to/terraform/configuration')
 
     expect(string_output.string).to(
-        include('DEBUG').and(
-            include("Running 'terraform plan some/path/to/terraform/configuration'.")))
+      include('DEBUG').and(
+        include(
+          "Running 'terraform plan some/path/to/terraform/configuration'."
+        )
+      )
+    )
   end
 
   it 'adds a var option for each supplied var' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var 'first=1' -var 'second=two' some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        vars: {
-            first: 1,
-            second: 'two'
-        })
+      directory: 'some/configuration',
+      vars:      {
+        first:  1,
+        second: 'two'
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          "terraform plan -var 'first=1' -var 'second=two' some/configuration",
+          any_args
+        )
+    )
   end
 
   it 'correctly serialises list/tuple vars' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var 'list=[1,\"two\",3]' some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        vars: {
-            list: [1, "two", 3]
-        })
+      directory: 'some/configuration',
+      vars:      {
+        list: [1, 'two', 3]
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          "terraform plan -var 'list=[1,\"two\",3]' some/configuration",
+          any_args
+        )
+    )
   end
 
   it 'correctly serialises map/object vars' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var 'map={\"first\":1,\"second\":\"two\"}' some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = "terraform plan -var 'map={\"first\":1,\"second\":\"two\"}' " \
+              'some/configuration'
 
     command.execute(
-        directory: 'some/configuration',
-        vars: {
-            map: {
-                first: 1,
-                second: "two"
-            }
-        })
+      directory: 'some/configuration',
+      vars:      {
+        map: {
+          first:  1,
+          second: 'two'
+        }
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'correctly serialises vars with lists/tuples of maps/objects' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var 'list_of_maps=[{\"key\":\"value\"},{\"key\":\"value\"}]' some/configuration", any_args))
+    command = described_class.new(binary: 'terraform')
+    message = "terraform plan -var 'list_of_maps=[{\"key\":\"value\"}," \
+              "{\"key\":\"value\"}]' some/configuration"
+    allow(Open4).to receive(:spawn)
 
     command.execute(
-        directory: 'some/configuration',
-        vars: {
-            list_of_maps: [
-                {key: "value"},
-                {key: "value"}
-            ]
-        })
+      directory: 'some/configuration',
+      vars:      {
+        list_of_maps: [
+          { key: 'value' },
+          { key: 'value' }
+        ]
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'adds a state option if a state path is provided' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -state=some/state.tfstate some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        state: 'some/state.tfstate')
+      directory: 'some/configuration',
+      state:     'some/state.tfstate'
+    )
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform plan -state=some/state.tfstate some/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds an out option if a plan path is provided' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -out=some/plan.tfplan some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        plan: 'some/plan.tfplan')
+      directory: 'some/configuration',
+      plan:      'some/plan.tfplan'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform plan -out=some/plan.tfplan some/configuration',
+          any_args
+        )
+    )
   end
 
   it 'includes the no-color flag when the no_color option is true' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform plan -no-color some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/path/to/terraform/configuration',
-        no_color: true)
+      directory: 'some/path/to/terraform/configuration',
+      no_color:  true
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform plan -no-color some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'includes the destroy flag when the destroy option is true' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform plan -destroy some/path/to/terraform/configuration', any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/path/to/terraform/configuration',
-        destroy: true)
+      directory: 'some/path/to/terraform/configuration',
+      destroy:   true
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform plan -destroy some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds a var-file option if a var file is provided' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var-file=some/vars.tfvars some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        var_file: 'some/vars.tfvars')
+      directory: 'some/configuration',
+      var_file:  'some/vars.tfvars'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(
+          'terraform plan -var-file=some/vars.tfvars some/configuration',
+          any_args
+        )
+    )
   end
 
   it 'adds a var-file option for each element of var-files array' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform plan -var-file=some/vars1.tfvars ' \
+              '-var-file=some/vars2.tfvars some/configuration'
+    command.execute(
+      directory: 'some/configuration',
+      var_files: [
+        'some/vars1.tfvars',
+        'some/vars2.tfvars'
+      ]
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var-file=some/vars1.tfvars -var-file=some/vars2.tfvars some/configuration", any_args))
-
-    command.execute(
-        directory: 'some/configuration',
-        var_files: [
-            'some/vars1.tfvars',
-            'some/vars2.tfvars'
-        ])
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'ensures that var_file and var_files options work together' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -var-file=some/vars.tfvars -var-file=some/vars1.tfvars -var-file=some/vars2.tfvars some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform plan -var-file=some/vars.tfvars ' \
+              '-var-file=some/vars1.tfvars ' \
+              '-var-file=some/vars2.tfvars some/configuration'
 
     command.execute(
-        directory: 'some/configuration',
-        var_file: 'some/vars.tfvars',
-        var_files: [
-            'some/vars1.tfvars',
-            'some/vars2.tfvars'
-        ])
+      directory: 'some/configuration',
+      var_file:  'some/vars.tfvars',
+      var_files: [
+        'some/vars1.tfvars',
+        'some/vars2.tfvars'
+      ]
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'adds a input option if a input value is provided' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -input=false some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        input: 'false')
+      directory: 'some/configuration',
+      input:     'false'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with('terraform plan -input=false some/configuration', any_args)
+    )
   end
 
   it 'adds a target option if a target is provided' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -target=some_resource_name some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        directory: 'some/configuration',
-        target: 'some_resource_name')
+      directory: 'some/configuration',
+      target:    'some_resource_name'
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+      .with(
+        'terraform plan -target=some_resource_name some/configuration',
+        any_args
+      )
+    )
   end
 
   it 'adds a target option for each element of target array' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -target=some_resource_1 -target=some_resource_2 some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform plan -target=some_resource_1 ' \
+              '-target=some_resource_2 ' \
+              'some/configuration'
 
     command.execute(
-        directory: 'some/configuration',
-        targets: [
-            'some_resource_1',
-            'some_resource_2'
-        ])
+      directory: 'some/configuration',
+      targets:   [
+        'some_resource_1',
+        'some_resource_2'
+      ]
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'ensures that target and targets options work together' do
-    command = RubyTerraform::Commands::Plan.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform plan -target=some_resource_1 -target=some_resource_2 -target=some_resource_3 some/configuration", any_args))
+    allow(Open4).to receive(:spawn)
+    command = described_class.new(binary: 'terraform')
+    message = 'terraform plan -target=some_resource_1 ' \
+              '-target=some_resource_2 ' \
+              '-target=some_resource_3 some/configuration'
 
     command.execute(
-        directory: 'some/configuration',
-        target: 'some_resource_1',
-        targets: [
-            'some_resource_2',
-            'some_resource_3'
-        ])
+      directory: 'some/configuration',
+      target:    'some_resource_1',
+      targets:   [
+        'some_resource_2',
+        'some_resource_3'
+      ]
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'logs an error raised when running the command' do
@@ -288,12 +393,12 @@ describe RubyTerraform::Commands::Plan do
     end
 
     stub_open4_spawn_raise
-    command = RubyTerraform::Commands::Plan.new
+    command = described_class.new
 
     begin
       command.execute(directory: 'some/path/to/terraform/configuration')
-    rescue
-      # no-op
+    rescue StandardError
+      nil
     end
 
     expect(string_output.string).to(
@@ -313,11 +418,11 @@ describe RubyTerraform::Commands::Plan do
     end
 
     stub_open4_spawn_raise
-    command = RubyTerraform::Commands::Plan.new
+    command = described_class.new
 
-    expect {
-      command.execute(directory: 'some/path/to/terraform/configuration')
-    }.to(raise_error(RubyTerraform::Errors::ExecutionError))
+    directory = 'some/path/to/terraform/configuration'
+    expect { command.execute(directory: directory) }
+      .to(raise_error(RubyTerraform::Errors::ExecutionError))
   end
 
   def stub_open4_spawn
@@ -326,10 +431,11 @@ describe RubyTerraform::Commands::Plan do
 
   def stub_open4_spawn_raise
     allow_any_instance_of(Process::Status).to(
-        receive(:exitstatus).and_return(0))
+      receive(:exitstatus).and_return(0)
+    )
     allow(Open4).to(
       receive(:spawn)
-        .and_raise(Open4::SpawnError.new("cmd", $?), "message")
+        .and_raise(Open4::SpawnError.new('cmd', $CHILD_STATUS), 'message')
     )
   end
 end
