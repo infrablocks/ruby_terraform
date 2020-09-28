252641ae4932d3115faa7b24fb00ec42912b7789
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:15:28 +0100

    Fix get_spec offenses

--- spec/ruby_terraform/commands/get_spec.rb
+++ spec/ruby_terraform/commands/get_spec.rb
@@ -1,57 +1,71 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe RubyTerraform::Commands::Get do
-  before(:each) do
+  let(:command) { described_class.new }
+  let(:terra_dir) { 'some/path/to/terraform/configuration' }
+
+  before do
     RubyTerraform.configure do |config|
       config.binary = 'path/to/binary'
     end
+    allow(Open4).to receive(:spawn)
   end
 
-  after(:each) do
+  after do
     RubyTerraform.reset!
   end
 
   it 'calls the terraform get command passing the supplied directory' do
-    command = RubyTerraform::Commands::Get.new(binary: 'terraform')
+    command = described_class.new(binary: 'terraform')
+
+    command.execute(directory: terra_dir)
 
     expect(Open4).to(
-        receive(:spawn)
-            .with('terraform get some/path/to/terraform/configuration', any_args))
-
-    command.execute(directory: 'some/path/to/terraform/configuration')
+      have_received(:spawn)
+        .with('terraform get some/path/to/terraform/configuration', any_args)
+    )
   end
 
   it 'defaults to the configured binary when none provided' do
-    command = RubyTerraform::Commands::Get.new
+    command.execute(directory: terra_dir)
 
     expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary get some/path/to/terraform/configuration', any_args))
-
-    command.execute(directory: 'some/path/to/terraform/configuration')
+      have_received(:spawn)
+        .with(
+          'path/to/binary get some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 
   it 'passes the update option as true when the update option is true' do
-    command = RubyTerraform::Commands::Get.new
+    message = 'path/to/binary get -update=true ' \
+              'some/path/to/terraform/configuration'
+    command.execute(
+      directory: terra_dir,
+      update:    true
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary get -update=true some/path/to/terraform/configuration', any_args))
-
-    command.execute(
-        directory: 'some/path/to/terraform/configuration',
-        update: true)
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'includes the no-color flag when the no_color option is true' do
-    command = RubyTerraform::Commands::Get.new
+    command.execute(
+      directory: terra_dir,
+      no_color:  true
+    )
 
     expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary get -no-color some/path/to/terraform/configuration', any_args))
-
-    command.execute(
-        directory: 'some/path/to/terraform/configuration',
-        no_color: true)
+      have_received(:spawn)
+        .with(
+          'path/to/binary get -no-color some/path/to/terraform/configuration',
+          any_args
+        )
+    )
   end
 end
