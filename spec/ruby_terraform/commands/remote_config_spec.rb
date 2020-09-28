da877d7f11d5231e0d0f73d328c246b50c452fd3
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:11:18 +0100

    Fix remote_config_spec offenses

--- spec/ruby_terraform/commands/remote_config_spec.rb
+++ spec/ruby_terraform/commands/remote_config_spec.rb
@@ -1,67 +1,86 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe RubyTerraform::Commands::RemoteConfig do
-  before(:each) do
+  before do
     RubyTerraform.configure do |config|
       config.binary = 'path/to/binary'
     end
   end
 
-  after(:each) do
+  after do
     RubyTerraform.reset!
   end
 
   it 'calls the terraform remote config command' do
-    command = RubyTerraform::Commands::RemoteConfig.new(binary: 'terraform')
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('terraform remote config', any_args))
+    command = described_class.new(binary: 'terraform')
 
     command.execute
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with('terraform remote config', any_args)
+    )
   end
 
   it 'defaults to the configured binary when none provided' do
-    command = RubyTerraform::Commands::RemoteConfig.new
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary remote config', any_args))
+    command = described_class.new
 
     command.execute
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with('path/to/binary remote config', any_args)
+    )
   end
 
   it 'uses the provided backend when supplied' do
-    command = RubyTerraform::Commands::RemoteConfig.new
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary remote config -backend=s3', any_args))
+    command = described_class.new
 
     command.execute(backend: 's3')
+
+    expect(Open4).to(
+      have_received(:spawn)
+          .with('path/to/binary remote config -backend=s3', any_args)
+    )
   end
 
   it 'adds a backend-config option for each supplied backend config' do
-    command = RubyTerraform::Commands::RemoteConfig.new(binary: 'terraform')
-
-    expect(Open4).to(
-        receive(:spawn)
-            .with("terraform remote config -backend-config 'thing=blah' -backend-config 'other=wah'", any_args))
+    allow(Open4).to receive(:spawn)
+    message = "terraform remote config -backend-config 'thing=blah' " \
+              "-backend-config 'other=wah'"
+    command = described_class.new(binary: 'terraform')
 
     command.execute(
-        backend_config: {
-            thing: 'blah',
-            other: 'wah'
-        })
+      backend_config: {
+        thing: 'blah',
+        other: 'wah'
+      }
+    )
+
+    expect(Open4).to(
+      have_received(:spawn)
+        .with(message, any_args)
+    )
   end
 
   it 'includes the no-color flag when the no_color option is true' do
-    command = RubyTerraform::Commands::RemoteConfig.new
+    allow(Open4).to receive(:spawn)
 
-    expect(Open4).to(
-        receive(:spawn)
-            .with('path/to/binary remote config -no-color', any_args))
+    command = described_class.new
 
     command.execute(no_color: true)
+
+    expect(Open4).to(
+      have_received(:spawn)
+          .with('path/to/binary remote config -no-color', any_args)
+    )
   end
 end
