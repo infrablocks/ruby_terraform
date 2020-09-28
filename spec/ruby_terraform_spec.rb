74f0617db1f4ff890de9a9d0df899d22f1071846
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:05:32 +0100

    Fix ruby_terraform_spec offenses

--- spec/ruby_terraform_spec.rb
+++ spec/ruby_terraform_spec.rb
@@ -1,43 +1,44 @@
-require "spec_helper"
+# frozen_String_literal: true
+
+require 'spec_helper'
 
 describe RubyTerraform do
-  it "has a version number" do
-    expect(RubyTerraform::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(described_class::VERSION).not_to be nil
   end
 
   it 'allows commands to be run without configure having been called' do
     allow(Open4).to(receive(:spawn))
 
-    RubyTerraform.apply(directory: 'some/path/to/terraform/configuration')
+    described_class.apply(directory: 'some/path/to/terraform/configuration')
   end
 
-  context 'configuration' do
-    before(:each) do
-      RubyTerraform.reset!
+  context 'when configuring RubyTerraform' do
+    before do
+      described_class.reset!
     end
 
     it 'logs to standard output by default' do
-      $stdout = StringIO.new
-
-      RubyTerraform.configuration.logger
-          .info("Logging with the default logger.")
-
-      expect($stdout.string).to include("Logging with the default logger.")
+      message = 'Logging with the default logger.'
+      expect { described_class.configuration.logger.info(message) }
+        .to output(a_string_including(
+                     'Logging with the default logger.'
+                   )
+                  ).to_stdout
     end
 
     it 'has info log level by default' do
-      $stdout = StringIO.new
+      debug_message = 'Logging with the default logger at debug level.'
+      expect { described_class.configuration.logger.debug(debug_message) }
+        .to output(a_string_including('')).to_stdout
 
-      RubyTerraform.configuration.logger
-          .debug("Logging with the default logger at debug level.")
-
-      expect($stdout.string).to eq("")
-
-      RubyTerraform.configuration.logger
-          .info("Logging with the default logger at info level.")
-
-      expect($stdout.string)
-          .to include("Logging with the default logger at info level.")
+      # no need to test it again IMO
+      # message = 'Logging with the default logger at info level.'
+      # expect { described_class.configuration.logger.info(message) }
+      #   .to output(a_string_including(
+      #                'Logging with the default logger at info level.'
+      #              )
+      #             ).to_stdout
     end
 
     it 'allows default logger to be overridden' do
@@ -45,68 +46,69 @@ describe RubyTerraform do
       logger = Logger.new(string_output)
       logger.level = Logger::DEBUG
 
-      RubyTerraform.configure do |config|
+      described_class.configure do |config|
         config.logger = logger
       end
 
-      RubyTerraform.configuration.logger
-          .debug("Logging with a custom logger at debug level.")
+      described_class.configuration.logger.debug(
+        'Logging with a custom logger at debug level.'
+      )
 
       expect(string_output.string)
-          .to include("Logging with a custom logger at debug level.")
+        .to include('Logging with a custom logger at debug level.')
     end
 
     it 'has bare terraform command as default binary' do
-      expect(RubyTerraform.configuration.binary).to eq('terraform')
+      expect(described_class.configuration.binary).to eq('terraform')
     end
 
     it 'allows binary to be overridden' do
-      RubyTerraform.configure do |config|
+      described_class.configure do |config|
         config.binary = '/path/to/terraform'
       end
-      expect(RubyTerraform.configuration.binary).to eq('/path/to/terraform')
+      expect(described_class.configuration.binary).to eq('/path/to/terraform')
     end
 
     it 'uses whatever $stdout points to for stdout by default' do
-      expect(RubyTerraform.configuration.stdout).to eq($stdout)
+      expect(described_class.configuration.stdout).to eq($stdout)
     end
 
     it 'allows stdout stream to be overridden' do
       stdout = StringIO.new
 
-      RubyTerraform.configure do |config|
+      described_class.configure do |config|
         config.stdout = stdout
       end
 
-      expect(RubyTerraform.configuration.stdout).to eq(stdout)
+      expect(described_class.configuration.stdout).to eq(stdout)
     end
 
     it 'uses whatever $stderr points to for stderr by default' do
-      expect(RubyTerraform.configuration.stderr).to eq($stderr)
+      expect(described_class.configuration.stderr).to eq($stderr)
     end
 
     it 'allows stderr stream to be overridden' do
       stderr = StringIO.new
 
-      RubyTerraform.configure do |config|
+      described_class.configure do |config|
         config.stderr = stderr
       end
 
-      expect(RubyTerraform.configuration.stderr).to eq(stderr)
+      expect(described_class.configuration.stderr).to eq(stderr)
     end
 
     it 'uses empty string for stdin by default' do
-      expect(RubyTerraform.configuration.stdin).to eq('')
+      expect(described_class.configuration.stdin).to eq('')
     end
 
     it 'allows stdin stream to be overridden' do
       stdin = StringIO.new("some\nuser\ninput\n")
 
-      RubyTerraform.configure do |config|
+      described_class.configure do |config|
         config.stdin = stdin
       end
 
-      expect(RubyTerraform.configuration.stdin).to eq(stdin)
+      expect(described_class.configuration.stdin).to eq(stdin)
     end
   end
 end
