546ad9e6354850c04296f91ee85d7738d2477c72
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-09-28 10:36:58 +0100

    Fix plan command offenses

--- lib/ruby_terraform/commands/plan.rb
+++ lib/ruby_terraform/commands/plan.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'lino'
 require_relative 'base'
 
@@ -18,28 +20,29 @@ module RubyTerraform
         no_color = opts[:no_color]
 
         builder
-            .with_subcommand('plan') do |sub|
-              vars.each do |key, value|
-                var_value = value.is_a?(String) ? value : JSON.generate(value)
-                sub = sub.with_option(
-                    '-var', "'#{key}=#{var_value}'", separator: ' ')
-              end
-              sub = sub.with_option('-var-file', var_file) if var_file
-              var_files.each do |file|
-                sub = sub.with_option('-var-file', file)
-              end
-              sub = sub.with_option('-target', target) if target
-              targets.each do |file|
-                sub = sub.with_option('-target', file)
-              end
-              sub = sub.with_option('-state', state) if state
-              sub = sub.with_option('-out', plan) if plan
-              sub = sub.with_option('-input', input) if input
-              sub = sub.with_flag('-destroy') if destroy
-              sub = sub.with_flag('-no-color') if no_color
-              sub
-            end
-            .with_argument(directory)
+          .with_subcommand('plan') do |sub|
+          vars.each do |key, value|
+            var_value = value.is_a?(String) ? value : JSON.generate(value)
+            sub = sub.with_option(
+              '-var', "'#{key}=#{var_value}'", separator: ' '
+            )
+          end
+          sub = sub.with_option('-var-file', var_file) if var_file
+          var_files.each do |file|
+            sub = sub.with_option('-var-file', file)
+          end
+          sub = sub.with_option('-target', target) if target
+          targets.each do |file|
+            sub = sub.with_option('-target', file)
+          end
+          sub = sub.with_option('-state', state) if state
+          sub = sub.with_option('-out', plan) if plan
+          sub = sub.with_option('-input', input) if input
+          sub = sub.with_flag('-destroy') if destroy
+          sub = sub.with_flag('-no-color') if no_color
+          sub
+        end
+          .with_argument(directory)
       end
     end
   end
