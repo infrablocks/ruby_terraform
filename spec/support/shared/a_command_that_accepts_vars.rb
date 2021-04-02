1debb9c2d71e312e2cbc3e81a83e47717a51eafd
Stephen Anderson <stephen.anderson@appstech.com.au>
2021-04-02 18:53:55 +1100

    Cleanup shared examples - pt1
    These shared examples are all used by Commands that include the directory parameter so making it mandatory and removing the argument logic.

--- spec/support/shared/a_command_that_accepts_vars.rb
+++ spec/support/shared/a_command_that_accepts_vars.rb
@@ -1,11 +1,7 @@
-# frozen_string_literal: true
-
-shared_examples 'a command that accepts vars' do |command, directory = nil|
-  argument = directory.nil? ? nil : " #{directory}"
-
+shared_examples 'a command that accepts vars' do |command, directory|
   it_behaves_like 'a valid command line', {
     reason: 'adds a var option for each supplied var',
-    expected_command: "terraform #{command} -var 'first=1' -var 'second=two'#{argument}",
+    expected_command: "terraform #{command} -var 'first=1' -var 'second=two' #{directory}",
     options: { directory: directory,
                vars: {
                  first: 1,
@@ -15,7 +11,7 @@ shared_examples 'a command that accepts vars' do |command, directory = nil|
 
   it_behaves_like 'a valid command line', {
     reason: 'correctly serialises list/tuple vars',
-    expected_command: "terraform #{command} -var 'list=[1,\"two\",3]'#{argument}",
+    expected_command: "terraform #{command} -var 'list=[1,\"two\",3]' #{directory}",
     options: { directory: directory,
                vars: {
                  list: [1, 'two', 3]
@@ -24,7 +20,7 @@ shared_examples 'a command that accepts vars' do |command, directory = nil|
 
   it_behaves_like 'a valid command line', {
     reason: 'correctly serialises map/object vars',
-    expected_command: "terraform #{command} -var 'map={\"first\":1,\"second\":\"two\"}'#{argument}",
+    expected_command: "terraform #{command} -var 'map={\"first\":1,\"second\":\"two\"}' #{directory}",
     options: { directory: directory,
                vars: {
                  map: {
@@ -36,7 +32,7 @@ shared_examples 'a command that accepts vars' do |command, directory = nil|
 
   it_behaves_like 'a valid command line', {
     reason: 'correctly serialises vars with lists/tuples of maps/objects',
-    expected_command: "terraform #{command} -var 'list_of_maps=[{\"key\":\"value\"},{\"key\":\"value\"}]'#{argument}",
+    expected_command: "terraform #{command} -var 'list_of_maps=[{\"key\":\"value\"},{\"key\":\"value\"}]' #{directory}",
     options: { directory: directory,
                vars: {
                  list_of_maps: [
--- spec/support/shared/a_command_that_can_disable_backup.rb
+++ spec/support/shared/a_command_that_can_disable_backup.rb
@@ -1,10 +1,8 @@
-shared_examples 'a command that can disable backup' do |command, directory = nil|
-  argument = directory.nil? ? nil : " #{directory}"
-
+shared_examples 'a command that can disable backup' do |command, directory|
   it_behaves_like 'a valid command line', {
     reason: 'disables backup if no_backup is true',
-    expected_command: "terraform #{command} -backup=- #{argument}",
-    options: { directory: argument,
+    expected_command: "terraform #{command} -backup=- #{directory}",
+    options: { directory: directory,
                backup: 'some/state.tfstate.backup',
                no_backup: true }
   }
