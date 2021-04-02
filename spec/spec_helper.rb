2111697bd525b9e0bc3b03a989fc03e783156a36
Stephen Anderson <stephen.anderson@appstech.com.au>
2021-04-02 18:51:48 +1100

    Ratchet coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'simplecov'
 
 SimpleCov.start do
   enable_coverage :branch
-  minimum_coverage line: 98.0, branch: 96.3
+  minimum_coverage line: 98.0, branch: 98.7
 end
 
 require 'bundler/setup'
