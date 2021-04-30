32e0ad9e6b1b8b8fe14ab660b44a7e4e29a7f78f
Toby Clemson <tobyclemson@gmail.com>
2021-04-30 22:18:31 +0100

    Document the validate command.

--- lib/ruby_terraform.rb
+++ lib/ruby_terraform.rb
@@ -1162,9 +1162,53 @@ module RubyTerraform
       exec(RubyTerraform::Commands::Untaint, parameters)
     end
 
+    # Invokes the +terraform validate+ command which checks whether a
+    # configuration is valid.
+    #
+    # Validates the configuration files in a directory, referring only to the
+    # configuration and not accessing any remote services such as remote state,
+    # provider APIs, etc.
+    #
+    # Validate runs checks that verify whether a configuration is syntactically
+    # valid and internally consistent, regardless of any provided variables or
+    # existing state. It is thus primarily useful for general verification of
+    # reusable modules, including correctness of attribute names and value
+    # types.
+    #
+    # It is safe to run this command automatically, for example as a post-save
+    # check in a text editor or as a test step for a re-usable module in a CI
+    # system.
+    #
+    # Validation requires an initialized working directory with any referenced
+    # plugins and modules installed. To initialize a working directory for
+    # validation without accessing any configured remote backend, use the
+    # {#init} command passing +:backend+ as +false+.
+    #
+    # To verify configuration in the context of a particular run (a particular
+    # target workspace, input variable values, etc), use the {#plan} command
+    # instead, which includes an implied validation check.
+    #
+    # @param parameters The parameters used to invoke the command
+    # @option parameters [String] :directory The directory containing terraform
+    #   configuration (deprecated).
+    # @option parameters [String] :chdir The path of a working directory to
+    #   switch to before executing the given subcommand.
+    # @option parameters [Boolean] :json (false) Whether or not to produce
+    #   output in a machine-readable JSON format, suitable for use in text
+    #   editor integrations and other automated systems; always disables color.
+    # @option parameters [Boolean] :no_color (false) Whether or not the output
+    #   from the command should be in color.
+    #
+    # @example Basic Invocation
+    #   RubyTerraform.validate(
+    #     directory: 'infra/networking')
+    #
+    def validate(parameters = {})
+      exec(RubyTerraform::Commands::Validate, parameters)
+    end
+
     {
       clean: RubyTerraform::Commands::Clean,
-      validate: RubyTerraform::Commands::Validate,
       workspace_list: RubyTerraform::Commands::WorkspaceList,
       workspace_select: RubyTerraform::Commands::WorkspaceSelect,
       workspace_new: RubyTerraform::Commands::WorkspaceNew,
--- lib/ruby_terraform/commands/validate.rb
+++ lib/ruby_terraform/commands/validate.rb
@@ -5,13 +5,60 @@ require_relative '../options/common'
 
 module RubyTerraform
   module Commands
+    # Wraps the +terraform validate+ command which checks whether a
+    # configuration is valid.
+    #
+    # Validates the configuration files in a directory, referring only to the
+    # configuration and not accessing any remote services such as remote state,
+    # provider APIs, etc.
+    #
+    # Validate runs checks that verify whether a configuration is syntactically
+    # valid and internally consistent, regardless of any provided variables or
+    # existing state. It is thus primarily useful for general verification of
+    # reusable modules, including correctness of attribute names and value
+    # types.
+    #
+    # It is safe to run this command automatically, for example as a post-save
+    # check in a text editor or as a test step for a re-usable module in a CI
+    # system.
+    #
+    # Validation requires an initialized working directory with any referenced
+    # plugins and modules installed. To initialize a working directory for
+    # validation without accessing any configured remote backend, use the {Init}
+    # command passing +:backend+ as +false+.
+    #
+    # To verify configuration in the context of a particular run (a particular
+    # target workspace, input variable values, etc), use the {Plan} command
+    # instead, which includes an implied validation check.
+    #
+    # For options accepted on construction, see {#initialize}.
+    #
+    # When executing an instance of {Validate} via {#execute}, the following
+    # options are supported:
+    #
+    # * +:directory+: the directory containing terraform configuration
+    #   (deprecated).
+    # * +:chdir+: the path of a working directory to switch to before executing
+    #   the given subcommand.
+    # * +:json+: whether or not to produce output in a machine-readable JSON
+    #   format, suitable for use in text editor integrations and other automated
+    #   systems; always disables color; defaults to +false+.
+    # * +:no_color+: whether or not the output from the command should be in
+    #   color; defaults to +false+.
+    #
+    # @example Basic Invocation
+    #   RubyTerraform::Commands::Validate.new.execute(
+    #     directory: 'infra/networking')
+    #
     class Validate < Base
       include RubyTerraform::Options::Common
 
+      # @!visibility private
       def subcommands
         %w[validate]
       end
 
+      # @!visibility private
       def options
         %w[
           -json
@@ -19,12 +66,9 @@ module RubyTerraform
         ] + super
       end
 
+      # @!visibility private
       def arguments(parameters)
         [parameters[:directory]]
-      end
-
-      def parameter_defaults(_parameters)
-        { vars: {}, var_files: [] }
       end
     end
   end
