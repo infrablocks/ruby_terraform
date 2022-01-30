a6841925a4ef63dad6734375a56fb93598a3794a
Toby Clemson <tobyclemson@gmail.com>
2022-01-30 17:39:47 +0000

    Add missing requires.

--- lib/ruby_terraform/options/values/complex.rb
+++ lib/ruby_terraform/options/values/complex.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 require_relative 'base'
 
 module RubyTerraform
--- lib/ruby_terraform/options/values/key_value.rb
+++ lib/ruby_terraform/options/values/key_value.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'immutable-struct'
+
 module RubyTerraform
   module Options
     module Values
